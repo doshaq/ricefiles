4f8032572e318a86d37577e6d57023ae7b6a051a
dosha <azdy68@gmail.com>
2018-11-01 23:24:26 +0300

    forgot polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,13 @@
+#!/usr/bin/env sh
 
+# Terminate already running bar instances
 killall -q polybar
 
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-while pgrep -x polybar >/dev/null; do sleep 2; done
+# Launch bar1 and bar2
+polybar white &
+#polybar bar2 &
 
-# Launch top bar
-polybar top 2> /dev/null &
-sleep 2
-polybar bottom 2> /dev/null &
+echo "Bars launched..."
